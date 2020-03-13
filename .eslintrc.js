9c0f360e64546aab1f2e36dae24a07b8348c6feb
Claudius Ibn <ibnclaudius@gmail.com>
2020-03-13 20:16:31 -0300

    Add some style guide tools (#2)
    * Add some style guide tools
    
    * Use root import
    
    * Change comment position

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'airbnb',
+    '@react-native-community',
+    'prettier/react',
+    'prettier',
+  ],
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: [
+          '.js',
+          '.jsx'
+        ],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
