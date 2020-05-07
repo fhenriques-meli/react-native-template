f214347bfed134fd5d74cecbfed97c887df46844
Romulo Mont Serrat <romulomontserrat@gmail.com>
2020-05-06 23:47:59 -0300

    change name

--- android/app/src/main/java/com/zibenlabs/moodlight/MainApplication.java
+++ android/app/src/main/java/com/zibenlabs/moodlight/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativetemplate;
+package com.zibenlabs.moodlight;
 
 import android.app.Application;
 import android.content.Context;
--- ios/MoodLightTests/MoodTests.m
+++ ios/MoodLightTests/MoodTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeTemplateTests : XCTestCase
+@interface MoodTests : XCTestCase
 
 @end
 
-@implementation ReactNativeTemplateTests
+@implementation MoodTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
